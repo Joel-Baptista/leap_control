678b46508f44056fe6d4b0f199a0c467e7c12fe7
Joel-Baptista <joelbaptista@ua.pt>
2025-12-20 13:33:06 +0000

    Configured Moveit planning

--- leap_moveit/launch/rsp.launch.py
+++ leap_moveit/launch/rsp.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_rsp_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("onshape", package_name="leap_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("leap", package_name="leap_moveit").to_moveit_configs()
     return generate_rsp_launch(moveit_config)
--- leap_moveit/launch/static_virtual_joint_tfs.launch.py
+++ leap_moveit/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("onshape", package_name="leap_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("leap", package_name="leap_moveit").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
